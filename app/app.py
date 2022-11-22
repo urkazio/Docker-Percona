849ecff254b52af1f2d3a199f840ed43180cb2ea
Endika Eiros <endika.eiros@gmail.com>
2022-11-22 17:41:39 +0100

    A

--- app/app.py
+++ app/app.py
@@ -12,6 +12,9 @@ def get_count():
     retries = 5
     while True:
         try:
+            db = mysql.connector.connect(
+                        host = 'percona-db', user = 'root', password = 'root', port = 3306, database = 'test')
+            print("Conexion establecida")
             cursor = db.cursor()
             cursor.execute("Select * from test_table")
             num = cursor.fetchall()
